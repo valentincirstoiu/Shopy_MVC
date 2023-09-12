df5ecfe5618d8900ae97717c0f527445491800e8
Nitnelav <Nitnelav@DESKTOP-D24CAEG>
2023-09-12 12:49:40 +0300

    update

--- Shopy.DataAccess/Migrations/20230825094324_addImageUrlToProduct.Designer.cs
+++ Shopy.DataAccess/Migrations/20230825094324_addImageUrlToProduct.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shopy.DataAccess.Data;
 
@@ -10,9 +11,11 @@ using Shopy.DataAccess.Data;
 namespace Shopy.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230825094324_addImageUrlToProduct")]
+    partial class addImageUrlToProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Shopy.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,40 @@
+﻿using Shopy.DataAccess.Data;
+using Shopy.DataAccess.Repository.IRepository;
+using Shopy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopy.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Product obj)
+        {
+            var objFromDb=_db.Products.FirstOrDefault(u=>u.Id == obj.Id);
+            if(objFromDb != null)
+            {
+                objFromDb.Title = objFromDb.Title;
+                objFromDb.ISBN = objFromDb.ISBN;
+                objFromDb.Price = objFromDb.Price;
+                objFromDb.Price50 = objFromDb.Price50;
+                objFromDb.ListPrice = objFromDb.ListPrice;
+                objFromDb.Price100 = objFromDb.Price100;
+                objFromDb.Description = objFromDb.Description;
+                objFromDb.Category = objFromDb.Category;
+                objFromDb.Author = objFromDb.Author;
+                if(obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
+        }
+    }
+}
